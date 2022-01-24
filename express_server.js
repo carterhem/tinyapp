581c54eb01cca2f3dbcbd3068a6891a138132a1d
Carter Hemington <carter.hemington@gmail.com>
2022-01-24 20:12:35 +0000

    add a route for /urls

--- express_server.js
+++ express_server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -18,9 +23,9 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
