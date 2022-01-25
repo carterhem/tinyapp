9a0cc9463d3cbe9f1637159936fa44f37e1ae6e0
Carter Hemington <carter.hemington@gmail.com>
2022-01-25 15:06:56 +0000

    linting!

--- express_server.js
+++ express_server.js
@@ -2,44 +2,42 @@ const { json } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
-
+const bodyParser = require("body-parser");
 
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
 app.set("view engine", "ejs");
 
 function generateRandomString() {
-let result = "";
-//place to put end string
-const possibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-//what characters am i choosing from?
-for (let j = 0; j < 6; j++) {
-  //loop through the string possibleCharacters
-  result += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
-  //add the character picked at random(mathfloor/mathrandom * length) to the result
-
-} return result;
+  let result = "";
+  //place to put end string
+  const possibleCharacters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  //what characters am i choosing from?
+  for (let j = 0; j < 6; j++) {
+    //loop through the string possibleCharacters
+    result += possibleCharacters.charAt(
+      Math.floor(Math.random() * possibleCharacters.length)
+    );
+    //add the character picked at random(mathfloor/mathrandom * length) to the result
+  }
+  return result;
 }
 
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
+//const bodyParser = require("body-parser"); moved to top for continuity
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -54,41 +52,38 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { 
-    shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL] 
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
 });
 
-
 app.post("/urls", (req, res) => {
-  console.log(req.body)
-  const longURL = req.body.longURL;// added longURL to this chain
-  const shortURL = generateRandomString()
+  console.log(req.body);
+  const longURL = req.body.longURL; // added longURL to this chain
+  const shortURL = generateRandomString();
 
-  urlDatabase[shortURL] = longURL
-  // urlDatabase = {...urlDatabase, [shortURL]:longURL}
+  urlDatabase[shortURL] = longURL;
+  //instead of making this hard - make the value of shortURL key in database longURL
   //spread operator to save the key value pair to the database
   // console.log("urldatabase", urlDatabase)
   // const ABC = {
   //   shortURL:longURL
-  console.log(urlDatabase)
+  console.log(urlDatabase);
   // console.log("ABC", ABC)
   // console.log("req.body.longURL", req.body.longURL);  // Log the POST request body to the console
-  // urlDatabase[shortURL]; 
+  // urlDatabase[shortURL];
   // console.log(urlDatabase)
   // console.log("shortURL", shortURL)
-  res.redirect(`/urls/${shortURL}`); 
+  res.redirect(`/urls/${shortURL}`);
   // console.log("redirect","/urls/:shortURL")
 });
-
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   console.log(longURL);
   res.redirect(longURL);
-
 });
 
 app.listen(PORT, () => {
