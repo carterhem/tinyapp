d00927c21162777153b4ff4602bfbc0700087be0
Carter Hemington <carter.hemington@gmail.com>
2022-01-25 23:34:59 +0000

    display the username functionality added

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 8080; //default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
 
 let urlDatabase = {
@@ -33,11 +34,13 @@ function generateRandomString() {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  // console.log(req)
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -57,6 +60,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
@@ -96,7 +100,7 @@ app.post("/login", (req, res) => {
 console.log(req.body)
 // console.log(req.body.username)
 res.cookie('username', req.body.username)
-// console.log(username)
+console.log(req.cookies)
 res.redirect("/urls")
   });
 
