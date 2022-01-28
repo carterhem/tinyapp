64932a592fa3e3a4e173149c1120fcff04d0caac
Carter Hemington <carter.hemington@gmail.com>
2022-01-28 00:29:59 +0000

    refactored Post register and login, can now login correctly

--- express_server.js
+++ express_server.js
@@ -4,33 +4,34 @@ const app = express();
 const PORT = 8080; //default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const bcrypt = require('bcryptjs');
-const salt = bcyrpt.genSaltSycn(10)
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
 app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: false }));
 
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
+    userID: "userRandomID",
   },
   i3BoGr: {
     longURL: "https://www.google.ca",
-    userID: "aJ48lW",
+    userID: "user2RandomID",
   },
 };
 
 const users = {
   userRandomID: {
     id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
+    email: "a@a.com",
+    password: bcrypt.hashSync("123", 10),
   },
   user2RandomID: {
     id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
+    email: "b@b.com",
+    password: bcrypt.hashSync("123", 10),
   },
 };
 
@@ -53,35 +54,34 @@ const generateRandomString = function () {
 const findEmail = function (email, users) {
   for (const key in users) {
     if (email === users[key].email) {
-      return email;
+      return users[key];
     }
   }
 };
 
 const findPassword = function (password, users) {
-  let returnKey = "";
+  let returnKey = [];
+  console.log("findPassword password", password);
   for (const key in users) {
-    if (password === users[key].password) {
+    console.log("findPassword users[key].password", users[key].password);
+    if (bcrypt.compareSync(password, users[key].password)) {
       returnKey = users[key];
     }
   }
+  console.log("findPassword returnKey", returnKey);
   return returnKey;
 };
 
-
 const urlsForUser = function (user, urlDatabase) {
- const userUrls = {};
-  for (const key in urlDatabase){
-   
+  const userUrls = {};
+  for (const key in urlDatabase) {
     if (user.id === urlDatabase[key].userID) {
-    // console.log("urlDatabase[key]", urlDatabase[key])
+      // console.log("urlDatabase[key]", urlDatabase[key])
       userUrls[key] = urlDatabase[key];
-    
     }
   }
   return userUrls;
 };
-
 
 app.get("/urls/new", (req, res) => {
   const user = users[req.cookies.user_id]
@@ -96,7 +96,11 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  // console.log(req)
+  console.log("at urls GET page");
+  console.log("URL GEt req.cookies", req.cookies);
+  console.log("URLS GET req.cookies.id", req.cookies.id);
+  // console.log("urls GET users", users)
+  // console.log("URLS GET users[req.cookies.id]", users[req.cookies.id] )
   const user = users[req.cookies.user_id]
     ? users[req.cookies.user_id]
     : undefined;
@@ -104,8 +108,7 @@ app.get("/urls", (req, res) => {
     return res.status(400).send("Please Login or Register to view this page!");
     //bug - messed up the users ability to logout (if they aren't logged in)
   }
-  console.log("at urls page")
-  const urlList = urlsForUser(user, urlDatabase)
+  const urlList = urlsForUser(user, urlDatabase);
   //now i have an array of shorturls and need to pull long urls from it to display
   // console.log("urlList", urlList)
   const templateVars = { urls: urlList, user: user };
@@ -163,37 +166,31 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = URL;
   //instead of making this hard - make the value of shortURL key in database longURL
   console.log("urlDatabase", urlDatabase);
-  console.log("users table", user)
+  console.log("users table", user);
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   // const user = users["key"];
-  console.log("req.cookies.user_id", req.cookies.user_id)
+  console.log("req.cookies.user_id", req.cookies.user_id);
   let cookieUser = req.cookies.user_id;
   //changed from const to let
 
-  console.log("urldatabase", urlDatabase)
-  console.log("urlDatabase[req.params.shortURL]", urlDatabase[req.params.shortURL])
-  // console.log("urlDatabase[req.params.shortURL].userID", urlDatabase[req.params.shortURL].userID)
-  console.log("cookieUser", cookieUser)
-  // const user = users[req.cookies.user_id];
-  // console.log("delete route - user", user)
-  // const urlList = urlsForUser(user, urlDatabase)
-  //what user is logged in right now
-  // console.log(urlDatabase[req.params.shortURL])
-  // console.log(urlDatabase[req.params.shortURL].userID)
-  // console.log(req.cookies.userRandomID)
-  // console.log(req.cookies.user_id)
-  if(cookieUser === urlDatabase[req.params.shortURL].userID) {
+  console.log("urldatabase", urlDatabase);
+  console.log(
+    "urlDatabase[req.params.shortURL]",
+    urlDatabase[req.params.shortURL]
+  );
+  
+  console.log("cookieUser", cookieUser);
+
+  if (cookieUser === urlDatabase[req.params.shortURL].userID) {
     delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls");
+    res.redirect("/urls");
   } else {
-    return res
-    .status(400)
-    .send("This URL does not belong to this User!");
+    return res.status(400).send("This URL does not belong to this User!");
   }
- res.redirect("/urls");
+  res.redirect("/urls");
 });
 
 app.post("/login", (req, res) => {
@@ -204,22 +201,23 @@ app.post("/login", (req, res) => {
     return res
       .status(400)
       .send("Email and/or password fields cannot be blank!");
-  } else if (!findEmail(loginEmail, users)) {
+  }
+
+  const user = findEmail(loginEmail, users);
+  if (!user) {
     //as in if there is no email found that matches form submission
     return res
       .status(403)
       .send("No user account exists for that email address!");
-  } else if (findEmail(loginEmail, users)) {
-    if (findPassword(loginPassword, users)) {
-      // console.log(findPassword(loginPassword, users));
-      const loggedInUser = findPassword(loginPassword, users);
-      const userID = loggedInUser.id;
-      req.cookies["userID"];
-      // console.log("redirecting...");
-      res.redirect("/urls");
-    } else {
-      return res.status(403).send("Incorrect Password");
-    }
+  }
+  console.log("=============", user)
+  const compare = bcrypt.compareSync(loginPassword, user.password);
+  if (compare) {
+    res.cookie("user_id", user.id);
+    console.log("redirecting from /login to /urls");
+    res.redirect("/urls");
+  } else {
+    return res.status(403).send("Incorrect Password");
   }
 });
 
@@ -230,19 +228,16 @@ app.post("/logout", (req, res) => {
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  console.log("req.cookies.user_id", req.cookies.user_id)
+  console.log("req.cookies.user_id", req.cookies.user_id);
   let cookieUser = req.cookies.user_id;
-  
-  if(cookieUser === urlDatabase[req.params.shortURL].userID) {
+
+  if (cookieUser === urlDatabase[req.params.shortURL].userID) {
     const newLongURL = req.body.newLongURL;
-  urlDatabase[req.params.shortURL].longURL = newLongURL;
-  res.redirect("/urls");
+    urlDatabase[req.params.shortURL].longURL = newLongURL;
+    res.redirect("/urls");
   } else {
-    return res
-    .status(400)
-    .send("This URL does not belong to this User!");
+    return res.status(400).send("This URL does not belong to this User!");
   }
- 
 });
 
 app.post("/register", (req, res) => {
@@ -268,8 +263,10 @@ app.post("/register", (req, res) => {
     const email = req.body.email;
     users[userRandomID]["email"] = email;
     const password = req.body.password;
-    users[userRandomID]["password"] = password;
-    console.log(users)
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    users[userRandomID]["password"] = hashedPassword;
+    console.log("REGISTER userRandomID", userRandomID);
+
     res.cookie("user_id", userRandomID);
     // console.log("users", users);
     res.redirect("/urls");
